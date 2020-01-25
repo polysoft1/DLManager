32eec2497c3ea19d741c708d905fccc9a80ffeea
jaredoconnell <jared.oc321@gmail.com>
2020-01-24 22:39:56 -0500

    Fixed linux filename. Added C++17 support to the test.

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -3,6 +3,9 @@
 #include <functional>
 #include <vector>
 #include <ctime>
+#if __cplusplus >= 201703L 
+#include <filesystem>
+#endif
 
 const int TOTAL_NUMS = 10000;
 const int CIEL = 500;
@@ -15,22 +18,26 @@ int main() {
 	function<void()> test;
 	function<double(std::vector<int>)> average;
 
+	std::string dyLibFileName = "test" + Polysoft::DLManager::getSuffix();
+
+	std::cout << "Testing basic functionality:" << std::endl;
 	/*
 	For linux it is important to give an absolute or relative path since
 	dlopen does not check the current directory if a path is not given
 	so in this case if "test.so" was passed to lib, it would not work as
-	anticipated
+	anticipated. See below to just use C++17 filesystem paths.
 	*/
-	Polysoft::DLManager lib("./test" + Polysoft::DLManager::getSuffix());
+	Polysoft::DLManager lib("./" + dyLibFileName);
 	Polysoft::DLManager lib2;
 	test = lib.getFunction<void()>("test");
 	test();
 	
-	lib2 = lib;
+	std::cout << std::endl << "Testing the copy function:" << std::endl;
+	lib2 = lib; // Test the = overload
 	lib.close();
 
 	std::vector<int> numbers;
-	for(int i = 0; i < TOTAL_NUMS; ++i){
+	for (int i = 0; i < TOTAL_NUMS; ++i) {
 		numbers.push_back(rand() % CIEL);
 	}
 
@@ -38,15 +45,30 @@ int main() {
 
 	std::cout << "Average of randoms is: " << average(numbers) << std::endl;
 
-	try{
+	std::cout << std::endl << "Testing fail cases:" << std::endl;
+	try {
 		lib2.getFunction<void()>("invalid_function_name");
-	}catch(Polysoft::NoSuchFunctionException e){
-		std::cout << "Error getting \"invalid_function_name\": " << e.what() << std::endl;
+	} catch (Polysoft::NoSuchFunctionException e) {
+		std::cout << "Intentional error getting \"invalid_function_name\": " << e.what() << std::endl;
 	}
 
-	try{
+	try {
 		lib.getFunction<void()>("test");
-	}catch(Polysoft::NoLibraryOpenException e){
-		std::cout << "Error getting \"test\": " << e.what() << std::endl;
+	} catch (Polysoft::NoLibraryOpenException e) {
+		std::cout << "Intentional error getting \"test\": " << e.what() << std::endl;
 	}
+
+#if __cplusplus >= 201703L 
+
+	// Test using filesystem
+	std::cout << std::endl << "Testing basic functionality with C++17 filesystem:" << std::endl;
+
+	std::filesystem::path path = std::filesystem::current_path() / dyLibFileName;
+	Polysoft::DLManager lib3(path);
+	test = lib3.getFunction<void()>("test");
+	test();
+#else
+	std::cout << std::endl << "C++17 not supported." << std::endl;
+#endif
+
 }
