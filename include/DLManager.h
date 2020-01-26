b4e34ee3c96e4bf1e57dbc524736b51203a3c946
jaredoconnell <jared.oc321@gmail.com>
2020-01-26 10:10:48 -0500

    Remove unnecessary include.
    I didn't update this earlier, and it wasn't even needed, so this should fix building on compilers with only partial support for C++17. Still no support for experimental filesystem, with no plans to add it.

--- include/DLManager.h
+++ include/DLManager.h
@@ -1,14 +1,6 @@
 #ifndef __DL_MANAGER_H__
 #define __DL_MANAGER_H__
 
-//Check for C++17 support
-#if __cplusplus >= 201703L
-#include <filesystem>
-#endif
-
-#include <string>
-#include "Exceptions.h"
-
 #ifdef _WIN32
 #include "DLManager_windows.h"
 #else 
